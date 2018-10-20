2491de5f65fdb88bbf75d7a486cd3d0c124fc019
EricZH <letItBezhumphries>
2018-10-20 12:51:32 -0700

    "Complete HoneyMakerBee"

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(honeyPot) {
+  Bee.call(this, honeyPot);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  return this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  return this.honeyPot--;
+};
